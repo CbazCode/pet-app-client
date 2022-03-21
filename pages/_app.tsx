8a4c47011f618ee98da570689fbc762090e3646a
Sebastian Asis <sasisromero10@gmail.com>
2022-03-21 16:51:52 -0500

    init project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {ChakraProvider} from '@chakra-ui/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Theme
+import theme from '../theme'
+
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
