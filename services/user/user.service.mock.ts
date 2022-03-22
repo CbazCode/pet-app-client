9fdcafa9730dd3f961cd8f216424c6d4e1093a3b
Sebastian Asis <sasisromero10@gmail.com>
2022-03-21 19:14:28 -0500

    change mock

--- services/user/user.service.mock.ts
+++ services/user/user.service.mock.ts
@@ -1,4 +1,7 @@
 // User services mock data
 import { User } from "./user.service.types";
 
-export const mockUser: User = {};
+export const mockUser: User = {
+  email: "juan@gmail.com",
+  password: "123456"
+};
